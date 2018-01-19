f23121b1ee02ccf88ba3d0228f7e4d938be7cf23
hexium310 <hexium310@gmail.com>
2018-01-20 03:51:57 +0900

    Migrate main to svelte

---
+++ src/main/components/AccountSelector.svelte
@@ -0,0 +1,9 @@
+Logged in:
+<span id="user-name" class="user-name">@{{ $users[$current_user_id].screenName }}</span>
+<input type="hidden" id="user-id" value="{{ $current_user_id }}">
+
+<style>
+  .user-name {
+    font-weight: bold;
+  }
+</style>
--- src/main/index.js
+++ src/main/index.js
@@ -1,8 +1,24 @@
+import { Store } from 'svelte/store'
+
 import * as Chrome  from '../utils/chrome'
-import './form'
+import AccountSelector from './components/AccountSelector.svelte'
+import TweetForm from './components/TweetForm.svelte'
 
 !(async () => {
   const { users } = await Chrome.Storage.Local.get('users')
-  document.querySelector('#username').insertAdjacentText('beforeend', users[Object.keys(users)[0]].screenName)
-  document.querySelector('#userid').setAttribute('value', Object.keys(users)[0])
+
+  const store = new Store({
+    current_user_id: Object.keys(users)[0],
+    users
+  })
+
+  new AccountSelector({
+    target: document.getElementById('account-selector'),
+    store
+  })
+
+  new TweetForm({
+    target: document.body.querySelector('#tweet-form'),
+    store
+  })
 })()
