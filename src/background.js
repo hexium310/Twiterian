e03c265169c10ef415a1d8cdb9aad635616f4a40
hexium310 <hexium310@gmail.com>
2018-01-20 17:49:07 +0900

    Migrate options to svelte

--- src/background.js
+++ src/background.js
@@ -1,8 +1,9 @@
 import twitter from 'twitter'
 
 import * as Chrome from './utils/chrome'
+import { postAccessToken, postRequestToken } from "./utils/twitter"
 
-Chrome.Runtime.onMessage.addListener(async message => {
+Chrome.Runtime.onMessage.addListener(async (message, sender) => {
   if (message.keys && message.tweet) {
     const { consumer_key, consumer_secret, access_token_key, access_token_secret } = message.keys
 
@@ -26,4 +27,63 @@ Chrome.Runtime.onMessage.addListener(async message => {
       media_ids: media.map(res => res.media_id_string).join(','),
     })
   }
+
+  switch (message.type) {
+    case 'AddAccount': {
+      const { consumer_key, consumer_secret } = require('../config')
+      const { oauthToken, oauthTokenSecret } = await postRequestToken(consumer_key, consumer_secret)
+
+      await Chrome.Storage.Local.set({
+        tokens: {
+          oauthToken,
+          oauthTokenSecret,
+        }
+      })
+
+      Chrome.Tabs.sendMessage(sender.tab.id, {
+        type: 'GotOAuthToken',
+      })
+
+      break
+    }
+
+    case 'GetAccessToken': {
+      const {
+        accessToken,
+        accessTokenSecret,
+        userId,
+        screenName
+      } = await postAccessToken(...Object.values(message.data))
+      const { users, count } = await Chrome.Storage.Local.get({users: {}, count: 0})
+
+      await Chrome.Storage.Local.set({
+        users: {
+          [userId]: {
+            screenName,
+            access_token: accessToken,
+            access_token_secret: accessTokenSecret,
+            orderBy: count,
+          },
+          ...(users),
+        },
+        count: count + 1,
+      })
+
+      Chrome.Tabs.sendMessage(sender.tab.id, {
+        type: 'AddSuccess',
+      })
+
+      break
+    }
+
+    case 'RemoveAccount': {
+      await Chrome.Storage.Local.set({
+        users: message.data.users
+      })
+
+      break
+    }
+  }
+
+  return true
 })
--- src/options/index.js
+++ src/options/index.js
@@ -1,77 +1,64 @@
 import querystring from 'querystring'
 
-import { postAccessToken, postRequestToken } from "../utils/twitter"
-import { consumer_key, consumer_secret } from '../../config'
-
 import * as Chrome  from '../utils/chrome'
-
-document.querySelector('#add-user button').addEventListener('click', async () => {
-  document.querySelector('#add-user button').setAttribute('disabled', '')
-  document.querySelector('#add-user button').classList.remove('icon-hover')
-
-  const { oauthToken, oauthTokenSecret } = await postRequestToken(consumer_key, consumer_secret)
-  await Chrome.Storage.Local.set({
-    tokens: {
-      oauthToken,
-      oauthTokenSecret,
-    }
-  })
-
-  await Chrome.Tabs.update({
-    url: `https://api.twitter.com/oauth/authorize?oauth_token=${oauthToken}`
-  })
-})
+import UserList from './components/UserList.svelte'
 
 if (!!window.location.search) {
-  const { oauth_token, oauth_verifier } = querystring.parse(window.location.search.substring(1))
+  const { oauth_token = null, oauth_verifier = null } = querystring.parse(window.location.search.substring(1))
+
+  history.replaceState(null, null, 'index.html')
 
   !(async () => {
+    const { consumer_key, consumer_secret } = require('../../config')
     const { tokens: { oauthToken, oauthTokenSecret } } = await Chrome.Storage.Local.get(['tokens'])
     if (oauth_token !== oauthToken) {
       return
     }
 
-    const { accessToken, accessTokenSecret, userId, screenName } = await postAccessToken(consumer_key, consumer_secret, oauthToken, oauthTokenSecret, oauth_verifier)
-    const { users, count } = await Chrome.Storage.Local.get({users: {}, count: 0})
-    await Chrome.Storage.Local.set({
-      users: {
-        [userId]: {
-          screenName,
-          access_token: accessToken,
-          access_token_secret: accessTokenSecret,
-          orderBy: count,
-        },
-        ...(users),
+    await Chrome.Runtime.sendMessage({
+      type: 'GetAccessToken',
+      data: {
+        consumer_key,
+        consumer_secret,
+        oauthToken,
+        oauthTokenSecret,
+        oauth_verifier
       },
-      count: count + 1,
-    })
-
-    await Chrome.Storage.Local.remove('tokens')
-
-    await Chrome.Tabs.update({
-      url: Chrome.Runtime.getURL('options/index.html')
     })
   })()
 }
 
 !(async () => {
   const { users } = await Chrome.Storage.Local.get('users')
-  for (const [index, [userid, { screenName }]] of Object.entries(users).sort(([, v], [, v2]) => v.orderBy - v2.orderBy).entries()) {
-    document.querySelector('#add-user').insertAdjacentHTML(
-      'beforebegin',
-      `<tr key=${index}>
-        <td><button class="delete icon-hover" value=${userid}><i class="fa fa-times fa-2x" aria-hidden="true"></i></button></td>
-        <td>${screenName}</td>
-      </tr>`
-    )
 
-    document.querySelector(`[key='${index}'] .delete`).addEventListener('click', async () => {
-      document.querySelector(`[key='${index}']`).remove()
-
-      delete users[userid]
-      await Chrome.Storage.Local.set({
-        users
-      })
-    })
-  }
+  new UserList({
+    target: document.querySelector('#main'),
+    data: {
+      users
+    }
+  })
 })()
+
+Chrome.Runtime.onMessage.addListener(async message => {
+  switch (message.type) {
+    case 'GotOAuthToken': {
+      const { tokens: { oauthToken } } = await Chrome.Storage.Local.get('tokens')
+
+      await Chrome.Tabs.update({
+        url: `https://api.twitter.com/oauth/authorize?oauth_token=${oauthToken}`
+      })
+
+      break
+    }
+
+    case 'AddSuccess': {
+      await Chrome.Storage.Local.remove('tokens')
+
+      await Chrome.Tabs.update({
+        url: Chrome.Runtime.getURL('options/index.html')
+      })
+    }
+  }
+
+  return true
+})
