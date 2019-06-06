e8de9201081e3b4dac455a80a0270703b8538f1d
Hexin <hexium310@gmail.com>
2019-06-06 18:19:45 +0900

    Add button of changing to next/previous user

--- src/main/components/AccountSelector.tsx
+++ src/main/components/AccountSelector.tsx
@@ -21,24 +21,40 @@ export const AccountSelector: React.FunctionComponent<AccountSelectorProps> = ({
   currentUserIndex,
   setCurrentUserIndex,
 }): React.ReactElement => {
-  const changeUser = async (event: React.ChangeEvent<HTMLSelectElement>): Promise<void> => {
-    const userIndex = Number(event.target.value);
+  const changeUser = async (index: number): Promise<void> => {
     await browser.storage.local.set({
-      currentUserIndex: userIndex,
+      currentUserIndex: index,
     });
-    setCurrentUserIndex(userIndex);
+    setCurrentUserIndex(index);
+  };
+
+  const changeToSelectedUser = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+    const userIndex = Number(event.target.value);
+    changeUser(userIndex);
+  };
+
+  const changeToNextUser = (): void => {
+    const userIndex = currentUserIndex + 1 !== users.length ? currentUserIndex + 1 : 0;
+    changeUser(userIndex);
+  };
+
+  const changeToPreviousUser = (): void => {
+    const userIndex = currentUserIndex - 1 >= 0 ? currentUserIndex - 1 : users.length - 1;
+    changeUser(userIndex);
   };
 
   return (
     <AccountSelectorWrapper>
       User
-      <Selector name="account" value={ currentUserIndex } onChange={ (event) => changeUser(event) }>
+      <Selector name="account" value={ currentUserIndex } onChange={ (event) => changeToSelectedUser(event) }>
         {
           users.map((user, index) => (
             <option key={ index } value={ index }>@{ user.screenName }</option>
           ))
         }
       </Selector>
+      <button onClick={ () => changeToNextUser() }>↓</button>
+      <button onClick={ () => changeToPreviousUser() }>↑</button>
     </AccountSelectorWrapper>
   );
 };
